2c5f27540a82536d980790ae0b7ddbcaabe29f07
rrs <contact@alligatr.co.uk>
2014-08-23 11:14:19 +0100

    Now uses template strings and a post template

--- convert.py
+++ convert.py
@@ -2,6 +2,8 @@ from markdown import markdown, markdownFromFile
 import sys
 import os
 import os.path as path
+import codecs
+from string import Template
 
 ext = ['codehilite', 'extra']
 
@@ -14,9 +16,10 @@ markdown_files = []
 markdown_path = path.join(path.abspath('.'), 'markdown')
 html_path = path.join(path.abspath('.'), 'html')
 
-template = open('template.html', 'ru').read()
-index = open('index.html', 'ru').read()
-lst = open('list.html', 'ru').read()
+template = Template(codecs.open('template.html', 'r' ,'utf-8').read())
+index    = Template(codecs.open('index.html', 'r', 'utf-8').read())
+post     = Template(codecs.open('post.html', 'r' ,'utf-8').read())
+lst      = Template(codecs.open('list.html', 'r', 'utf-8').read())
 
 for root, dirs, files in os.walk(path.join(path.abspath('.'), 'markdown')):
   d = path.basename(root)
@@ -24,13 +27,21 @@ for root, dirs, files in os.walk(path.join(path.abspath('.'), 'markdown')):
 
 markdown_files.sort(key=lambda x: x[1], reverse=rev)
 
+index_list = ''
 for d, fi in markdown_files:
   print 'writing', fi + '.html'
-  md = open(path.join(markdown_path, fi), 'r').read().decode('utf-8')
-  title = md.split('#')[1].split('\n')[0][1:]
-  html = markdown(md, extensions=ext).encode('utf-8')
-  html += '<div class="footer"><a href="/blog">Back</a></div>'
-  open(path.join(html_path, fi + '.html'), 'w').write(template.format(title, html))
-  index += lst.format('<a href="{}.html">{}</a>'.format(fi, title))
+  mapping = {}
+  md = codecs.open(path.join(markdown_path, fi), 'r', 'utf-8').read()
+  html = markdown(md, extensions=ext)
 
-open(path.join(html_path, 'index.html'), 'w').write(template.format('index', index))
+  mapping['title'] = md.split('#')[1].split('\n')[0][1:]
+  mapping['body'] = html
+  mapping['date'] = fi[:10]
+  mapping['url'] = fi + '.html'
+
+  post_html = template.substitute(title=mapping['title'], body=post.substitute(mapping))
+  codecs.open(path.join(html_path, fi + '.html'), 'w', 'utf-8').write(post_html)
+  if not fi.endswith('.draft'):
+    index_list += lst.substitute(mapping)
+
+codecs.open(path.join(html_path, 'index.html'), 'w', 'utf-8').write(template.substitute(title='index', body=index.substitute({'list':index_list})))
